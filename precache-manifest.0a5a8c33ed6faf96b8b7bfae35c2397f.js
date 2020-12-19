0c355ee9f899ced90dab8dc058de71dd03f03428
DRAKOS <drakostius2@gmail.com>
2020-12-19 12:14:41 +0200

    Updates

---
+++ precache-manifest.0a5a8c33ed6faf96b8b7bfae35c2397f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46a7214b41215c7bf4573e5ae1bc755",
+    "url": "/goit-react-hw-04-hooks-phonebook/index.html"
+  },
+  {
+    "revision": "2ea35c3c543cbac92d7f",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/css/main.917ff00f.chunk.css"
+  },
+  {
+    "revision": "09da18f6a33b71882857",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.5aeaa74f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/2.5aeaa74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea35c3c543cbac92d7f",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/main.996fae56.chunk.js"
+  },
+  {
+    "revision": "f17dac181506b7b4b1dc",
+    "url": "/goit-react-hw-04-hooks-phonebook/static/js/runtime-main.09b235dc.js"
+  }
+]);
